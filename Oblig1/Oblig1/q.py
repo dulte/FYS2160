4b60d55ee6b86eb6ab88b3ead460449b81ad5b33
dulte <daniel.heinesen@sf-nett.no>
2017-09-11 16:29:50 +0200

    Add files via upload

--- Oblig1/Oblig1/q.py
+++ Oblig1/Oblig1/q.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Sep 11 14:56:37 2017
+Created on Mon Sep 11 16:07:13 2017
 
 @author: dulte
 """
@@ -18,9 +18,20 @@ randomstates = np.random.randint(0,2,(M,N))
 microstates = np.where(randomstates==0, -1,randomstates)
 
 energies = -(np.sum(microstates == 1,axis=1) - np.sum(microstates == -1,axis=1))/2.
+            
+            
+def exact(N,s,O_max):
+    return O_max*np.exp(-2*s**2/float(N))
+
+
+omega_max = (factorial(N)/(factorial(N/2.))**2)*(M/2**N)
+s = np.linspace(-30,30,100)
+
 
 plt.hist(energies,33)
+plt.plot(s,exact(N,s,omega_max),label="Analytical")
 plt.title(r"Multiplisity $\Omega$ " + "for M = %g, N = %g" %(M,N))
 plt.xlabel(r"Energy $[\mu B]$")
 plt.ylabel(r"Multiplisity $\Omega$")
+plt.legend()
 plt.show()
